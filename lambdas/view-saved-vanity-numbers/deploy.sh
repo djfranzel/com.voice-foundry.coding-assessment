98fc6ffb2772f32cb5199c12af050ea7147a2974
David Franzel <djfranzel@pm.me>
2021-11-02 21:36:24 -0500

    updated deployment scripts, need to start cloud formation template

--- lambdas/view-saved-vanity-numbers/deploy.sh
+++ lambdas/view-saved-vanity-numbers/deploy.sh
@@ -8,3 +8,6 @@ zip -r lambda-deployment-package.zip . || exit 1
 
 echo "Updating Lambda function with updated code..."
 aws lambda update-function-code --function-name view-saved-vanity-numbers --zip-file fileb://lambda-deployment-package.zip --region us-east-1 --profile personal || exit 1
+
+echo "Deleting deployment package..."
+rm lambda-deployment-package.zip
